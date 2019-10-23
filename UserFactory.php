1c7ac413cb42501ffcebc19aaafe9c67d3723ec8
Mike Calvo <calvo.michael1@gmail.com>
2019-10-23 16:51:04 -0400

    updated UserClass functions and started User Factory

---
+++ UserFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * User Factory Class
+ * @author Mike Calvo, inspired by Ike Quigley 
+ */
+
+
+abstract class DataFactory{
+    const DATA_TABLE = "";
+    
+    
+    public function get_properties() {
+        //used to transport name-value pairs
+        $pairs = new SplObjectStorage();
+
+
+        
+    }
+}
