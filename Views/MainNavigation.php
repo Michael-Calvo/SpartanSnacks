a03147ab9e6d87da5c910b4c452c93318803b0aa
harinderb98 <54410181+harinderb98@users.noreply.github.com>
2019-11-06 17:23:58 -0500

    updated restaurant review to have ratings

--- Views/MainNavigation.php
+++ Views/MainNavigation.php
@@ -11,12 +11,15 @@ Updated: 10/25/2019
         // sets home link location based on if the current page is index.php or not
 
         if (basename($_SERVER['PHP_SELF']) == 'index.php') {
-            echo "<li><a href='index.php'>Home</a></li> <li><a href='./Views/AboutIndex.php'>About</a></li>";
+            echo "<li><a href='index.php'>Home</a></li> "
+               . "<li><a href='./Views/AboutIndex.php'>About</a></li>";
         } else if (basename($_SERVER['PHP_SELF']) == 'SubmissionCatcher.php'){
-            echo "<li><a href='../index.php'>Home</a></li> <li><a href='../Views/AboutView.php'>About</a></li>";
+            echo "<li><a href='../index.php'>Home</a></li> "
+               . "<li><a href='../Views/AboutView.php'>About</a></li>";
         }
         else {
-            echo "<li><a href='../index.php'>Home</a></li> <li><a href='./AboutView.php'>About</a></li>";
+            echo "<li><a href='../index.php'>Home</a></li> "
+               . "<li><a href='./AboutView.php'>About</a></li>";
         }
         ?>
     </ul>    
