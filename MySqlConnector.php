e413987e2b9753c4429dba8326a47aff4438904e
calvo <calvo.michael1@gmail.com>
2019-11-10 20:00:44 -0500

    Added files for database. Need completion

--- MySqlConnector.php
+++ MySqlConnector.php
@@ -1,35 +1,31 @@
 <?php
 
+class mySqlConnector implements DataBaseInterface{
 //Author: Mike Calvo
-
-
 //These Need Values!
-$dataName;
-$serverName ;
-$passowrd;
-$username;
-
+const $dataName;
+const $serverName;
+const $passowrd;
+const $username;
 //To create the connection 
 $connect = new mysqli($servername, $username, $password, $dataName);
 
 //Connection Checking, if there is a connection error, print the error
-if($connect->connect_error){
+if ($connect->connect_error) {
     exit("Failure" . $connect->connect_error);
 }
 
-//Adding values to the database. Needs values
-$sql = "INSERT INTO #######
-    VALUES (#####)";
-
-//Testing to make sure the item was added. This tests and puts onto the page. 
-// Create a debugger to put into a console. not page. 
-
-if($conenct->query($sql)=== TRUE){
-    echo "Sucess! Record Added!";
-}else{
-    echo "Whoops! ".$sql. "<br>" . $connect->error;
+public function createObject(array $keypair, $table){
+    $query = "ISNERT INTO " + $table;
+    $names = "(";
+    $values = "VALUES (";
+    
+    
+    
 }
-   
+
+
 
 //Closing the Connection
-$connect -> close();
+$connect->close();
+}
