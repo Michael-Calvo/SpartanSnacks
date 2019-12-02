4049a50f065945fa3d468bddbc9cdf8a1808d0a9
Mike Calvo <calvo.michael1@gmail.com>
2019-12-01 21:48:59 -0500

    added datastoreadapter stuff

--- Database/DataStoreAdapter.php
+++ Database/DataStoreAdapter.php
@@ -1,19 +1,38 @@
 <?php
+//author Mike Calvo, Ike Quigly
+require_once 'Database/mySqlConnector.php';
+require_once 'Database/NewUserSearch.php';
+
 class DataStoreAdapter{
+    //This fuction is made to create an instance of the mySqlConnector
+    public function _createConnector(){
+    $this->connector = new MySQLConnector();
+    }
+    //This is used to create a new object using the data from the NewUserSearch
+    //Then put it through the MySqlConnector so it can be put in the database
     public function createObject(){
-        
+        $id = $this->connector->createObject($newSearch);
+
+
     }
-    public function readObject(){
-        
+    //Suppose to read object. Ike creates a generic type object and returns it.
+    //Not sure why.
+    public function readObject($map){
+        $results = $this->connector->readObject($map, $table);
+
+
     }
-    public function updateObject(){
-        
+    //returns the update object for the sql connector using a
+    //get Properties from the data factory, the UUID, and a getDataTable.
+    //How to do properties and datatable is still in the air.
+    public function updateObject($obj){
+        return $this->connector->updateObject($DUMMY, $obj.getUuid(),$DUMMY);
     }
-    public function deleteObject(){
-        
+    public function deleteObject($obj){
+        return DataStoreAdapter::updateObject($obj);
     }
-    
-    
-    
-    
+
+
+
+
 }
