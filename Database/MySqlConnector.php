e0218b44d57d5b7659423118c6543e4d7278fb0f
calvo <calvo.michael1@gmail.com>
2019-12-02 16:51:33 -0500

    fix

--- Database/MySqlConnector.php
+++ Database/MySqlConnector.php
@@ -2,21 +2,18 @@
 
 class mySqlConnector implements DataBaseInterface {
 
-//Author: Mike Calvo
-//These Need Values!
+//Author: Mike Calvo, Ike Quigley
+//Connects to the database
     public function connectToDatabase() {
 
-        $host = "DUMMY";
-        $databaseName = "DUMMY";
-        $passowrd = "DUMMY";
-        $username = "DUMMY";
-//To create the connection
+        $host = "localhost";
+        $databaseName = "spartansnacks";
+        $password = " ";
+        $username = "root";
+        //To create the connection
         $connect = new mysqli($host, $username, $password, $databaseName);
 
-
-
-
-//Connection Checking, if there is a connection error, print the error
+        //Connection Checking, if there is a connection error, print the error
         if ($connect->connect_error) {
             exit("Failure" . $connect->connect_error);
         }
@@ -24,22 +21,22 @@ class mySqlConnector implements DataBaseInterface {
         return $connect;
     }
 
-    public function createObject(array $keypair, $table) {
-        $query = "ISNERT INTO " + $table;
-        $names = "(";
-        $values = "VALUES (";
+    //Creates a new entry to the database
+    public function createObject($databaseName) {
+
+        $query = "INSERT INTO " + $databaseName;
     }
 
     public function deleteObject($uuid) {
-
+        
     }
 
     public function updateObject(array $keypair, $uuid, $table) {
-
+        
     }
 
     public function readOject(array $keypair, $table) {
-
+        
     }
 
 }
